aa02a2ab5e2203ab16dad428fb3c4ce9c11b12b9
asie <kontakt@asie.pl>
2020-08-01 08:14:47 +0200

    [atari_tos] support higher screen resolutions, mono

--- src/frontend/atari_tos/src/main.c
+++ src/frontend/atari_tos/src/main.c
@@ -34,6 +34,11 @@
 #include "zoo_sound_pcm.h"
 #include "zoo_ui.h"
 
+enum graphics_mode {
+	GFX_MODE_MONO,
+	GFX_MODE_COLOR16
+};
+
 extern uint8_t _4x8_bin[];
 static MFDB font_4x8_m;
 static MFDB screen_m;
@@ -41,6 +46,8 @@ static MFDB screen_m;
 static short wk_work_in[11], wk_work_out[57];
 static short wk_handle;
 
+static enum graphics_mode gfx_mode;
+static short x_offset, y_offset;
 static short old_pal_colors[16 * 3];
 static short pal_colors[16 * 3] = {
 	0, 0, 0,
@@ -71,15 +78,19 @@ static void init_gfx(void) {
 	font_4x8_m.fd_wdwidth = font_4x8_m.fd_w >> 4;
 	font_4x8_m.fd_nplanes = 1;
 
-	for (short i = 0; i < 16; i++) {
-		vq_color(wk_handle, i, 0, old_pal_colors + (i * 3));
-		vs_color(wk_handle, i, pal_colors + (i * 3));
+	if (gfx_mode == GFX_MODE_COLOR16) {
+		for (short i = 0; i < 16; i++) {
+			vq_color(wk_handle, i, 0, old_pal_colors + (i * 3));
+			vs_color(wk_handle, i, pal_colors + (i * 3));
+		}
 	}
 }
 
 static void deinit_gfx(void) {
-	for (short i = 0; i < 16; i++) {
-		vs_color(wk_handle, i, old_pal_colors + (i * 3));
+	if (gfx_mode == GFX_MODE_COLOR16) {
+		for (short i = 0; i < 16; i++) {
+			vs_color(wk_handle, i, old_pal_colors + (i * 3));
+		}
 	}
 }
 
@@ -92,18 +103,26 @@ static void vdi_write_char(zoo_video_driver *drv, int16_t x, int16_t y, uint8_t 
 	short coords[8];
 	short colors[2];
 
+	if (gfx_mode == GFX_MODE_COLOR16) {
+		colors[0] = col & 15;
+		colors[1] = (col >> 4) & 7;
+	} else {
+		uint8_t fg = col & 15;
+		uint8_t bg = (col >> 4) & 7;
+
+		colors[0] = fg != 0 ? 0 : 1;
+		colors[1] = (fg == 0 && bg != 0) ? 0 : 1;
+	}
+
+	coords[4] = (x << 2) + x_offset;
+	coords[5] = (y << 3) + y_offset;
+	coords[6] = coords[4] + 3;
+	coords[7] = coords[5] + 7;
+
 	coords[0] = (chr & 31) << 2;
 	coords[1] = (chr >> 5) << 3;
 	coords[2] = coords[0] + 3;
 	coords[3] = coords[1] + 7;
-
-	coords[4] = x << 2;
-	coords[5] = y << 3;
-	coords[6] = coords[4] + 3;
-	coords[7] = coords[5] + 7;
-
-	colors[0] = col & 15;
-	colors[1] = col >> 4;
 
 	vrt_cpyfm(wk_handle, 1, coords, &font_4x8_m, &screen_m, colors);
 }
@@ -118,8 +137,18 @@ int main(int argc, char **argv) {
 	wk_work_in[10] = 2;
 
 	v_opnvwk(wk_work_in, &wk_handle, wk_work_out);
-	v_exit_cur(wk_handle);
 
+	// TODO: crashes
+	/* if (wk_work_out[0] < 319 || wk_work_out[1] < 199) {
+		form_alert(1, "[3][Cannot launch:|Resolution too low!][ OK ]");
+		goto FinishWk;
+	} */
+
+	x_offset = (wk_work_out[0] + 1 - 320) >> 1;
+	y_offset = (wk_work_out[1] + 1 - 200) >> 1;
+	gfx_mode = wk_work_out[13] >= 16 ? GFX_MODE_COLOR16 : GFX_MODE_MONO;
+
+	v_exit_cur(wk_handle);
 	init_gfx();
 
 	zoo_state_init(&state);
@@ -135,6 +164,7 @@ int main(int argc, char **argv) {
 	zoo_ui_init(&ui_state, &state);
 
 	bool running = true;
+	short tdelay = 55;
 	while (running) {
 		short kmeta;
 		short kreturn;
@@ -144,7 +174,7 @@ int main(int argc, char **argv) {
 			0, 0, 0, 0, 0,
 			0, 0, 0, 0, 0,
 			NULL,
-			55,
+			tdelay,
 			NULL, NULL, NULL,
 			NULL, &kreturn,
 			NULL
@@ -201,10 +231,11 @@ int main(int argc, char **argv) {
 					case RETURN_IMMEDIATE:
 						break;
 					case RETURN_NEXT_FRAME:
-						// tick_delay = 16;
+						tdelay = 16;
 						ticking = false;
 						break;
 					case RETURN_NEXT_CYCLE:
+						tdelay = 55;
 						ticking = false;
 						break;
 				}
@@ -212,9 +243,9 @@ int main(int argc, char **argv) {
 		}
 	}
 
-FinishWk:
 	deinit_gfx();
 
+FinishWk:
 	v_clsvwk(wk_handle);
 
 	appl_exit();
